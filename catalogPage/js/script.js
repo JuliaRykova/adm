780369437198192ead2f4859b548f3ec07f456bf
Ваше Имя ADM <nikitin@adm-center.ru>
2024-03-18 16:12:02 +0300

    доработала

--- catalogPage/js/script.js
+++ catalogPage/js/script.js
@@ -1,8 +1,7 @@
-document.getElementById("open").addEventListener("click", function() {
-  document.getElementById("hiddenBlokCatalog").style.display = "flex";
+document.getElementById('open').addEventListener('click', function () {
+  document.getElementById('hiddenBlokCatalog').style.display = 'flex';
 });
 
-
-document.getElementById("open-text").addEventListener("click", function() {
-  document.getElementById("transparent-text").style.display = "none";
+document.getElementById('open-text').addEventListener('click', function () {
+  document.getElementById('transparent-text').style.display = 'none';
 });
