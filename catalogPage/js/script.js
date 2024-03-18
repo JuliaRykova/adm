a7884ac91c19468fb09b09f90da1998b27498fd2
Ваше Имя ADM <nikitin@adm-center.ru>
2024-03-18 16:11:07 +0300

    добавила js

---
+++ catalogPage/js/script.js
@@ -0,0 +1,8 @@
+document.getElementById("open").addEventListener("click", function() {
+  document.getElementById("hiddenBlokCatalog").style.display = "flex";
+});
+
+
+document.getElementById("open-text").addEventListener("click", function() {
+  document.getElementById("transparent-text").style.display = "none";
+});
